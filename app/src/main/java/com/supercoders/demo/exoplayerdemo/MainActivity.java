3ea43388cee403a9ca82914e4a37067098adecd6
ibrahimgharyali <ibrahimgharyali@gmail.com>
2017-12-07 21:15:41 +0530

    milestone

--- app/src/main/java/com/supercoders/demo/exoplayerdemo/MainActivity.java
+++ app/src/main/java/com/supercoders/demo/exoplayerdemo/MainActivity.java
@@ -1,12 +1,13 @@
 package com.supercoders.demo.exoplayerdemo;
 
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.media.MediaCodec;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.support.annotation.NonNull;
-import android.support.v4.content.PermissionChecker;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,7 +16,6 @@ import android.widget.TextView;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.LoadControl;
@@ -26,13 +26,11 @@ import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.decoder.DecoderCounters;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.extractor.ExtractorsFactory;
-import com.google.android.exoplayer2.extractor.mp4.Mp4Extractor;
-import com.google.android.exoplayer2.mediacodec.MediaCodecSelector;
 import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.LoopingMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.TrackGroupArray;
-import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
@@ -43,16 +41,12 @@ import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
-import com.google.android.exoplayer2.video.MediaCodecVideoRenderer;
 import com.google.android.exoplayer2.video.VideoRendererEventListener;
-
-import java.security.Permission;
 
 public class MainActivity extends AppCompatActivity implements VideoRendererEventListener {
 
     private static final String TAG = "MainActivity";
     private static final int REQUEST_WRITE_PERMISSION = 345;
-    private SimpleExoPlayerView simpleExoPlayerView;
     private SimpleExoPlayer player;
     private TextView resolutionTextView;
     boolean durationSet = false;
@@ -62,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements VideoRendererEven
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         requestPermission();
-        resolutionTextView = new TextView(this);
+//        resolutionTextView = new TextView(this);
         resolutionTextView = (TextView) findViewById(R.id.resolution_textView);
 
 // 1. Create a default TrackSelector
@@ -75,8 +69,8 @@ public class MainActivity extends AppCompatActivity implements VideoRendererEven
 
 // 3. Create the player
         player = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
-        simpleExoPlayerView = new SimpleExoPlayerView(this);
-        simpleExoPlayerView = (SimpleExoPlayerView) findViewById(R.id.player_view);
+//        simpleExoPlayerView = new SimpleExoPlayerView(this);
+        SimpleExoPlayerView simpleExoPlayerView = (SimpleExoPlayerView) findViewById(R.id.player_view);
 
 //Set media controller
         simpleExoPlayerView.setUseController(true);
@@ -107,30 +101,6 @@ public class MainActivity extends AppCompatActivity implements VideoRendererEven
         }
     }
     private void fetchResourceAndStart() {
-//        I. ADJUST HERE:
-//CHOOSE CONTENT: LiveStream / SdCard
-
-//LIVE STREAM SOURCE: * Livestream links may be out of date so find any m3u8 files online and replace:
-
-//        Uri mp4VideoUri2 =Uri.parse("http://81.7.13.162/hls/ss1/index.m3u8"); //random 720p source
-//        Uri mp4VideoUri =Uri.parse("http://54.255.155.24:1935//Live/_definst_/amlst:sweetbcha1novD235L240P/playlist.m3u8"); //Radnom 540p indian channel
-//
-// Uri mp4VideoUri =Uri.parse("FIND A WORKING LINK ABD PLUg INTO HERE"); //PLUG INTO HERE<------------------------------------------
-
-
-//VIDEO FROM SD CARD: (2 steps. set up file and path, then change videoSource to get the file)
-        String urimp1 = "/Download/video/s_1.mp4"; //upload file to device and add path/name.mp4
-        String urimp2 = "/Download/video/s_2.3gp"; //upload file to device and add path/name.mp4
-        String urimp3 = "/Download/video/s_3.mp4"; //upload file to device and add path/name.mp4
-        String urimp4 = "/Download/video/s_4.mp4"; //upload file to device and add path/name.mp4
-//        Uri mp4VideoUri = Uri.parse(Environment.getExternalStorageDirectory().getAbsolutePath()+urimp4);
-
-        /*Uri videoUri1 = Uri.parse(Environment.getExternalStorageDirectory().getAbsolutePath()+urimp1);
-        Uri videoUri2 = Uri.parse(Environment.getExternalStorageDirectory().getAbsolutePath()+urimp2);
-        Uri videoUri3 = Uri.parse(Environment.getExternalStorageDirectory().getAbsolutePath()+urimp3);
-        Uri videoUri4 = Uri.parse(Environment.getExternalStorageDirectory().getAbsolutePath()+urimp4);*/
-
-
 
 //Measures bandwidth during playback. Can be null if not required.
         DefaultBandwidthMeter bandwidthMeterA = new DefaultBandwidthMeter();
@@ -140,42 +110,35 @@ public class MainActivity extends AppCompatActivity implements VideoRendererEven
         ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
 
 
-// II. ADJUST HERE:
+//PLUG INTO HERE<------------------------------------------
 
+//VIDEO FROM SD CARD: (2 steps. set up file and path, then change videoSource to get the file)
         String[] uris = new String[]{
                 "/Download/video/s_1.mp4",
                 "/Download/video/s_2.3gp",
                 "/Download/video/s_3.mp4",
                 "/Download/video/s_4.mp4"
         };
+
+
         Uri[] videoUris = new Uri[uris.length];
+
+
+        //This is the MediaSource representing the media to be played:
         MediaSource[] videoSources = new MediaSource[uris.length];
         for (int i = 0; i < uris.length; i ++ ) {
             String vurl =uris[i];
             videoUris[i] = Uri.parse(Environment.getExternalStorageDirectory().getAbsolutePath() + vurl);
             videoSources[i] = new ExtractorMediaSource(videoUris[i], dataSourceFactory, extractorsFactory, null, null);
         }
-//This is the MediaSource representing the media to be played:
-//FOR SD CARD SOURCE:
-        /*MediaSource videoSource1 = new ExtractorMediaSource(videoUri1, dataSourceFactory, extractorsFactory, null, null);
-        MediaSource videoSource2 = new ExtractorMediaSource(videoUri2, dataSourceFactory, extractorsFactory, null, null);
-        MediaSource videoSource3 = new ExtractorMediaSource(videoUri3, dataSourceFactory, extractorsFactory, null, null);
-        MediaSource videoSource4 = new ExtractorMediaSource(videoUri4, dataSourceFactory, extractorsFactory, null, null);*/
 
-
-//FOR LIVESTREAM LINK:
-//        MediaSource videoSource = new HlsMediaSource(videoUri3, dataSourceFactory, 1, null, null);
-//        MediaSource videoSource2 = new HlsMediaSource(videoUri4, dataSourceFactory, 1, null, null);
-//        MergingMediaSource mergingMediaSource = new MergingMediaSource(videoSource);
         final ConcatenatingMediaSource mediaSource = new ConcatenatingMediaSource(true,videoSources);
 
-
-//        final LoopingMediaSource loopingSource = new LoopingMediaSource(mediaSource);
+// Looping mediaSource indefinitely
+        final LoopingMediaSource loopingSource = new LoopingMediaSource(mediaSource);
 
 // Prepare the player with the source.
-        player.prepare(mediaSource);
-//        player.prepare(videoSource1);
-
+        player.prepare(loopingSource);
         player.addListener(new Player.EventListener() {
             @Override
             public void onTimelineChanged(Timeline timeline, Object manifest) {
@@ -210,8 +173,24 @@ public class MainActivity extends AppCompatActivity implements VideoRendererEven
             public void onPlayerError(ExoPlaybackException error) {
                 Log.v(TAG, "Listener-onPlayerError...");
                 player.stop();
-                player.prepare(mediaSource);
-                player.setPlayWhenReady(true);
+                AlertDialog.Builder builder;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder = new AlertDialog.Builder(MainActivity.this, android.R.style.Theme_Material_Dialog_Alert);
+                }
+                else {
+                    builder = new AlertDialog.Builder(MainActivity.this);
+                }
+                builder.setMessage(getResources().getString(R.string.copy_video_dialog_message));
+                builder.setTitle(getResources().getString(R.string.not_found_title));
+                builder.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                        finish();
+                    }
+                });
+                builder.create().show();
+
             }
 
             @Override
